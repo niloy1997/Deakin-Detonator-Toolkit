c7f5d2b5e98d670f5e07b80188b7c6ec67a44912
Christian <russo-christian@proton.me>
2024-07-25 17:42:18 +1000

    add method in install script to check for rust, volta, nodejs, and yarn, and only install if not present.

--- install-update-media/install-ddt.py
+++ install-update-media/install-ddt.py
@@ -4,7 +4,7 @@ import subprocess
 import time
 import os
 
-def run_command(command, message, sudo=False):
+def run_command(command, message, sudo=False, return_output=False):
     """
     Executes a shell command.
 
@@ -12,9 +12,12 @@ def run_command(command, message, sudo=False):
         command (str): The command to be executed.
         message (str):The message to show to the user of the current step.
         sudo (bool, optional): Whether to run the command with sudo. Defaults to False.
+        return_output (bool, optional): Whether to return command output. Defaults to False.
+    
+    Returns:
+        tuple: return code and output of the command if return_output is True.
     """
 
-    # Initialise global variables within the function.
     global TOTAL_STEPS
     global DEBUG
     global current_step
@@ -22,32 +25,22 @@ def run_command(command, message, sudo=False):
     # Print the loading bar.
     print_loading_bar(current_step, message, TOTAL_STEPS)
 
-    # Run the command.
-    # If debug mode is enabled, show the output.
-    if DEBUG:
+    # Add optional sudo
+    if sudo:
+        command = f"sudo {command}"
 
-        # If sudo is enabled, run the command with sudo.
-        if sudo:
-            subprocess.run(f"sudo {command}", shell=True)
-
-        # If sudo is not enabled, run the command without sudo.
-        else:
-            subprocess.run(command, shell=True)
-
-    # If debug mode is not enabled, hide the output.
+    # Run the command and optionally capture output.
+    if DEBUG or return_output:
+        result = subprocess.run(command, shell=True, capture_output=return_output, text=True)
     else:
-
-        # If sudo is enabled, run the command with sudo.
-        if sudo:
-            subprocess.run(f"sudo {command}", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-        # If sudo is not enabled, run the command without sudo.
-        else:
-            subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        result = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     # Increment the current step counter.
-    # This is used to update the loading bar.
     current_step += 1
+
+    # Return the result if requested.
+    if return_output:
+        return result.returncode, result.stdout.strip()
 
 def print_loading_bar(iteration, message, total, length=50):
     """
@@ -144,6 +137,41 @@ def check_sudo():
     # If the user ID is 0, the script is running with sudo.
     return os.geteuid() == 0
 
+def install_prereq(name, check_command, install_command, path_variable, sleep_time_duration):
+    """
+    Function to check if a prerequisite software is installed and install it if not.
+
+    Args:
+        name (str): The name of the software.
+        check_command (str): The command to check if the software is installed.
+        install_command (str): The command to install the software.
+        path_variable (str): The path variable to set after installation.
+        sleep_time_duration (int): The duration to wait after installation.
+    """
+    global current_step
+
+    def check_installed():
+        result = run_command(check_command, f"Checking if {name} is installed...", return_output=True)
+        return_code = result[0]
+        return return_code == 0
+
+    def install():
+        global current_step
+        message = f"Installing {name}..."
+        new_terminal(install_command, message, f"{name} Install Terminal")
+
+        message = f"Setting {name} PATH variable..."
+        print_loading_bar(current_step, message, TOTAL_STEPS)
+        os.environ["PATH"] = f"{os.path.expanduser('~')}/{path_variable}:{os.environ['PATH']}"
+        current_step += 1
+
+        sleep_time(sleep_time_duration, f"Allowing time for {name} to install")
+
+    if not check_installed():
+        install()
+    else:
+        print(f"{name} is already installed. Skipping {name} installation.")
+
 # Establish global variables.
 global TOTAL_STEPS 
 global DEBUG
@@ -152,7 +180,7 @@ global current_step
 
 # Initialise constants.
 TOTAL_STEPS = 13    # Total number of steps
-DEBUG = False       # Whether to show debug information.
+DEBUG = True       # Whether to show debug information.
 SLEEP_TIME = 20     # The number of seconds to sleep for.
 
 # Initialise local variables.
@@ -203,47 +231,41 @@ run_command("apt upgrade --fix-missing -y", message, True)
 message = "Installing the required software dependencies..."
 run_command(f"apt install {' '.join(packages)} -y", message, True)
 
-# Install Rust.
-message = "Installing Rust..."
-new_terminal('curl --proto "=https" --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y', message, "Rust Install Terminal")
+# Check and install Rust
+install_prereq(
+    name="Rust",
+    check_command="rustc --version",
+    install_command='curl --proto "=https" --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y',
+    path_variable=".cargo/bin",
+    sleep_time_duration=SLEEP_TIME
+)
 
-# Add Rust to PATH.
-# This allows running Rust commands from the terminal without restarting.
-message = "Setting Rust PATH variable..."
-print_loading_bar(current_step, message, TOTAL_STEPS)
-os.environ["PATH"] = f"{os.path.expanduser('~')}/.cargo/bin:{os.environ['PATH']}"
-current_step += 1
+# Check and install Volta
+install_prereq(
+    name="Volta",
+    check_command="volta --version",
+    install_command='curl https://get.volta.sh | bash',
+    path_variable=".volta/bin",
+    sleep_time_duration=SLEEP_TIME
+)
 
-# TODO: Introduce a method to check if it installed rather than waiting specified time.
-sleep_time(SLEEP_TIME, "Allowing time for Rust to install")
+# Check and install Node.js
+install_prereq(
+    name="Node.js",
+    check_command="node --version",
+    install_command='volta install node',
+    path_variable=".volta/bin",
+    sleep_time_duration=SLEEP_TIME
+)
 
-# Install Volta.
-message = "Installing Volta..."
-new_terminal('curl https://get.volta.sh | bash', message, "Volta Install Terminal")
-
-# Set the PATH variable for the current session.
-# This allows running Volta commands from the terminal without restarting.
-message = "Setting Volta PATH variable..."
-print_loading_bar(current_step, message, TOTAL_STEPS)
-os.environ["PATH"] = f"{os.path.expanduser('~')}/.volta/bin:{os.environ['PATH']}"
-current_step += 1
-
-# TODO: Introduce a method to check if it installed rather than waiting specified time.
-sleep_time(SLEEP_TIME, "Allowing time for volta to install")
-
-# Install Node.
-message = "Installing Node..."
-new_terminal('volta install node', message, "Node Install Terminal")
-
-# TODO: Introduce a method to check if it installed rather than waiting specified time.
-sleep_time(SLEEP_TIME, "Allowing time for NODE to install")
-
-# Install Yarn.
-message = "Installing Yarn..."
-new_terminal('volta install yarn', message, "Yarn Install Terminal")
-
-# TODO: Introduce a method to check if it installed rather than waiting specified time.
-sleep_time(SLEEP_TIME, "Allowing time for YARN to install")
+# Check and install Yarn
+install_prereq(
+    name="Yarn",
+    check_command="yarn --version",
+    install_command='volta install yarn',
+    path_variable=".volta/bin",
+    sleep_time_duration=SLEEP_TIME
+)
 
 os.chdir("./Deakin-Detonator-Toolkit")
 message = "Starting the Deakin Detonator Toolkit..."
